1cf44c62740faa4f645e6f210b962868d56017c9
northkuria <kuri4martin@gmail.com>
2022-06-15 12:36:45 +0300

    requests

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+const submitData = (name, email) => {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      const node = document.createElement('h1')
+      const text = document.createTextNode(data.id)
+      node.appendChild(text)
+      document.querySelector('body').appendChild(node)
+    })
+    .catch(function (error) {
+      const node = document.createElement('h1')
+      const text = document.createTextNode(error.message)
+      node.appendChild(text)
+      document.querySelector('body').appendChild(node)
+    })
+}
